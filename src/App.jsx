8d953cd27d1ef7542527693d03b98edead3ee2de
BenTAMUC <bsmithers1@leomail.tamuc.edu>
2023-11-02 18:38:57 -0500

    Got name, bio, and image displayed from json

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
 import './App.css'
 
+const did = {
+  "@context": [
+    "https://www.w3.org/ns/did/v1",
+    "https://w3id.org/security/suites/jws-2020/v1"
+],
+"id": "did:web:example.com",
+"verificationMethod": [
+    {
+    "id": "did:web:example.com:#key1",
+    "type": "Ed25519VerificationKey2020",
+    "controller": "did:web:example.com#key1",
+    "Ed25519VerificationKey2020" : "127928319239123812"
+    }
+],
+"assertionMethod": [
+    "did:web:example.com:#key1"
+],
+"subject": {
+    "name": "Jordan Schneider",
+    "bio": "I like trains",
+    "displayImg": "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+}
+}
+
 function App() {
+  const displayName = did.subject.name;
+  const bio = did.subject.bio;
+  const displayImg = did.subject.displayImg;
 
   return(
     <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-xl" id="main-content">
     <div class="flex flex-col md:flex-row items-center md:items-start">
 
         <div class="md:w-1/2 mb-6 md:mb-0 pr-0 md:pr-4 text-center md:text-left">
-            <div class="w-16 h-16 bg-gray-300 rounded-full mb-4 mx-auto md:mx-0"></div>
-            <h1 class="text-xl font-semibold mb-2">displayName</h1>
-            <p>bio</p>
+            <div class="w-16 h-16 bg-red-300 rounded-full mb-4 mx-auto md:mx-0">
+              <img src = {displayImg}></img>
+            </div>
+            <h1 class="text-xl font-semibold mb-2">{displayName}</h1>
+            <p>{bio}</p>
         </div>
 
 
@@ -44,7 +73,7 @@ function App() {
     </div>
 </div>
   ) 
-  }
+}
 
 
 export default App
