f39d68ada81ada1867057a4d50a9599d0056c2ef
BenTAMUC <bsmithers1@leomail.tamuc.edu>
2023-11-06 18:03:30 -0600

    Sample sigchain + sigchain page updates

--- src/Sigchain.jsx
+++ src/Sigchain.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import './Sigchain.css';
+import { Link } from 'react-router-dom';
+
+const sig = [
+    {
+        "seqno": 1,
+        "type": "genesis",
+        "data": {},
+        "signature": "00a1b2c3d4",
+        "prev": null
+    },
+    {
+        "seqno": 2,
+        "type": "verify",
+        "data": {},
+        "signature": "f1e2d3c4b5",
+        "prev": "00a1b2c3d4"
+    },
+    {
+        "seqno": 3,
+        "type": "verify",
+        "data": {},
+        "signature": "a1b2c3d4e5",
+        "prev": "f1e2d3c4b5"
+    },
+    {
+        "seqno": 4,
+        "type": "revoke",
+        "data": {},
+        "signature": "1a2b3c4d5e",
+        "prev": "a1b2c3d4e5"
+    }
+]
 
 const Sigchain = () => {
     return (
         <div className="activity-container">
-            <h2>Home</h2>
+            <Link to = {"/"}>
+                <h2>Home</h2>
+            </Link>
             <h3>Activity</h3>
             <ul>
                 <li className="highlighted">Claimed ownership of Github account</li>
@@ -24,7 +58,7 @@ const Sigchain = () => {
                 <p>Signed payload:</p>
                 {/* ... Your signed payload details ... */}
             </div>
-            <button>Download</button>
+            <button>Full JSON Download</button>
         </div>
     );
 }
