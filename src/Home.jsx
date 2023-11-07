01583dbd91511b447e0bf592835e261b4ed61330
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-11-07 17:38:05 -0600

    Converted cluttered annoying Tailwind to good CSS

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './Home.css'
 import { Link } from 'react-router-dom';
 import IdentityCard from './components/IdentityCard';
 import { useEffect, useState } from 'react';
@@ -75,32 +75,34 @@ function Home() {
     },[])
 
   return(
-    <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-xl" id="main-content">
-    <div class="flex flex-col md:flex-row items-center md:items-start">
+    <div className="main-container" id="main-content">
+    <div className="content-wrapper">
 
-        <div class="md:w-1/2 mb-6 md:mb-0 pr-0 md:pr-4 text-center md:text-left">
-            <div class="w-16 h-16 bg-red-300 rounded-full mb-4 mx-auto md:mx-0">
-              <img src = {displayImg}></img>
+        <div className="left-panel">
+            <div className="image-container">
+              <img src={displayImg} alt=""></img>
             </div>
-            <h1 class="text-xl font-semibold mb-2">{displayName}</h1>
-            <p>{bio}</p>
+            <h1 className="title">{displayName}</h1>
+            <p className="bio">{bio}</p>
         </div>
 
-
-        <div class="md:w-1/2 text-center md:text-left">
-            <div class="mb-4 flex-row">
-                <Link to={"Sigchain"} class="text-black hover:underline">Sigchain<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" /></svg></Link>
+        <div className="right-panel">
+            <div id="link-container">
+                <Link to={"Sigchain"} className="sigchain-link">Sigchain
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="arrow-icon">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" /></svg>
+                </Link>
             </div>
 
-            <div>
-                <ul>
+            <div className="identities-container">
+                <ul className="identities-list">
                     {identitiesList}
                 </ul>
             </div>
         </div>
     </div>
 </div>
+
   ) 
 }
 
